1a9e56be29231482c89cd2e92418f6dd444ddba4
NEO <67116756+faizalkhan87@users.noreply.github.com>
2023-09-25 01:05:38 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -78,7 +78,7 @@ export default function Home() {
         </div>
         <form onSubmit={handleSubmit} className="flex-none p-6">
           <div className="flex rounded-lg border border-gray-700 bg-gray-800">
-            <input type="text" className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none" placeholder="Type your message..." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+            <input type="text" className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none" placeholder="Type your message...." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
             <button type="submit" className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300">Send</button>
           </div>
         </form>
