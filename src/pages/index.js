dc7cb44cf660853723f8fa57a06f0e0a2fe0c253
NEO <67116756+faizalkhan87@users.noreply.github.com>
2023-10-28 23:14:48 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import { useState, useEffect } from "react";
-import Head from 'next/head'
-import Image from 'next/image'
+import React, { useState } from "react";
+// import Head from 'next/head'
+// import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+// import styles from '@/styles/Home.module.css'
 import axios from 'axios';
 import TypingAnimation from "../components/TypeAnimation";
 
@@ -12,6 +12,9 @@ export default function Home() {
   const [inputValue, setInputValue] = useState('');
   const [chatLog, setChatLog] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isUser, setIsUser] = useState(false);
+
+
 
 
   const handleSubmit = (event) => {
@@ -24,17 +27,17 @@ export default function Home() {
     setInputValue('');
   }
 
-const sendMessage = (message) => {
-  const url = "https://api.openai.com/v1/chat/completions";
+  const sendMessage = (message) => {
+    const url = "https://api.openai.com/v1/chat/completions";
 
-  const headers = {
-    "Content-Type": 'application/json',
-    "Authorization": `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`
-  }
-  const data = {
-    "model": "gpt-3.5-turbo-16k-0613",
-    messages: [{ "role": "user", "content": message }]
-  };
+    const headers = {
+      "Content-Type": 'application/json',
+      "Authorization": `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`
+    }
+    const data = {
+      "model": "gpt-3.5-turbo-16k-0613",
+      messages: [{ "role": "user", "content": message }]
+    };
 
 
     setIsLoading(true);
@@ -48,43 +51,83 @@ const sendMessage = (message) => {
       console.log(error);
     })
   }
+  // React.useEffect(() => {
+  //   setTimeout(() => {
+  //     setIsUser(true)
+
+  //   }, 5000);
+  // })
 
   return (
-    <div className="container mx-auto max-w-[100%]">
-      <div className="flex flex-col  min-h-screen max-h-100% bg-gray-900">
-        <h1 className="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">Zelda ~Ai</h1>
 
-        <div className="flex-grow p-6">
-          <div className="flex flex-col space-y-4">
-            {
-              chatLog.map((message, index) => (
-                <div key={index} className={`flex ${message.type === 'user' ? 'justify-end' : 'justify-start'
-                  }`}>
-                  <div className={`${message.type === 'user' ? 'bg-purple-500' : 'bg-gray-800'
-                    } rounded-lg p-4 text-white max-w-sm`}>
-                    {message.message}
+
+
+    !isUser ?
+      <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+        <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+          <div
+            class="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+          </div>
+          <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+            <div class="max-w-md mx-auto">
+              <div>
+                <h1 class="text-2xl font-semibold">Login with Zelda-Ai</h1>
+              </div>
+              <div class="divide-y divide-gray-200">
+                <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                  <div class="relative">
+                    <input type="email" autocomplete="off" id="email" name="email" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
+                    <label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
+                  </div>
+                  <div class="relative">
+                    <input autocomplete="off" id="password" name="password" type="password" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
+                    <label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                  </div>
+                  <div class="relative">
+                    <button class="bg-blue-500 text-white rounded-md px-2 py-1" onClick={() => setIsUser(true)}>Submit</button>
                   </div>
                 </div>
-              ))
-            }
-            {
-              isLoading &&
-              <div key={chatLog.length} className="flex justify-start">
-                <div className="bg-gray-800 rounded-lg p-4 text-white max-w-sm">
-                  <TypingAnimation />
-                </div>
               </div>
-            }
+            </div>
           </div>
         </div>
-        <form onSubmit={handleSubmit} className="flex-none p-6">
-          <div className="flex rounded-lg border border-gray-700 bg-gray-800">
-            <input type="text" className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none" placeholder="Type your message...." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-            <button type="submit" className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300">Send</button>
-          </div>
-        </form>
-
       </div>
-    </div>
+      :
+      <div className="container mx-auto max-w-[100%]">
+        <div className="flex flex-col  min-h-screen max-h-100% bg-gray-900">
+          <h1 className="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">Zelda ~Ai</h1>
+
+          <div className="flex-grow p-6">
+            <div className="flex flex-col space-y-4">
+              {
+                chatLog.map((message, index) => (
+                  <div key={index} className={`flex ${message.type === 'user' ? 'justify-end' : 'justify-start'
+                    }`}>
+                    <div className={`${message.type === 'user' ? 'bg-purple-500' : 'bg-gray-800'
+                      } rounded-lg p-4 text-white max-w-sm`}>
+                      {message.message}
+                    </div>
+                  </div>
+                ))
+              }
+              {
+                isLoading &&
+                <div key={chatLog.length} className="flex justify-start">
+                  <div className="bg-gray-800 rounded-lg p-4 text-white max-w-sm">
+                    <TypingAnimation />
+                  </div>
+                </div>
+              }
+            </div>
+          </div>
+          <form onSubmit={handleSubmit} className="flex-none p-6">
+            <div className="flex rounded-lg border border-gray-700 bg-gray-800">
+              <input type="text" className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none" placeholder="Type your message..." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+              <button type="submit" className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300">Send</button>
+            </div>
+          </form>
+
+        </div>
+      </div>
   )
 }
